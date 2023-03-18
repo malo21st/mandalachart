070dd82277c3be6429f2ef59ce568e91ed3f4c76
malo21st <44739330+malo21st@users.noreply.github.com>
2023-03-18 15:02:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ if st.button('**マンダラート創造**') and title:
 if mandala_html:
     st.sidebar.download_button(
         label="CSVダウンロード",
-        data=mandal_csv,
+        data=mandla_csv,
         file_name='solution.csv',
         mime='text/csv',
     )
