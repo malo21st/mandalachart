fd49d0b1116c535de695a300a98a92818ab628bf
malo21st <44739330+malo21st@users.noreply.github.com>
2023-03-21 06:32:24 +0900

    Update app_svg.py

--- app_svg.py
+++ app_svg.py
@@ -11,17 +11,11 @@ st.set_page_config(
 #     page_icon = Image.open("favicon.png")
 )
 
+# OPEN AI API KEY
 openai.api_key = st.secrets['api_key']
 
+# Mandal Chart Layout
 ROW, COL, UNIT = 9, 9, 80
-
-AI_TYPE = {'きっちり': 0.0, 'まぁまぁ': 0.5, 'クリエイティブ': 0.9}
-
-# base, center, center group, outer_center
-C0, C1, C2, C3 = "white", "pink", "aqua", "aqua"
-COLOR = {(4, 4): C1, (3, 3): C2, (3, 4): C2, (3, 5): C2, (4, 3): C2, (4, 5): C2, (5, 3): C2, (5, 4): C2, (5, 5): C2,
-           (1, 1): C3, (1, 4): C3, (1, 7): C3, (4, 1): C3, (4, 7): C3, (7, 1): C3, (7, 4): C3, (7, 7): C3}
-
 MANDAL_LIST = [[ 9, 10, 11, 18, 19, 20, 27, 28, 29],
                [12, 13, 14, 21, 22, 23, 30, 31, 32],
                [15, 16, 17, 24, 25, 26, 33, 34, 35],
@@ -32,21 +26,21 @@ MANDAL_LIST = [[ 9, 10, 11, 18, 19, 20, 27, 28, 29],
                [57, 58, 59, 66, 67, 68, 75, 76, 77],
                [60, 61, 62, 69, 70, 71, 78, 79, 80]]
 
+# Cart Back Ground Color C0: default, C1: center, C2: center group, c3: outer_center
+C0, C1, C2, C3 = "white", "pink", "aqua", "aqua"
+COLOR = {(4, 4): C1, (3, 3): C2, (3, 4): C2, (3, 5): C2, (4, 3): C2, (4, 5): C2, (5, 3): C2, (5, 4): C2, (5, 5): C2,
+         (1, 1): C3, (1, 4): C3, (1, 7): C3, (4, 1): C3, (4, 7): C3, (7, 1): C3, (7, 4): C3, (7, 7): C3}
+
+# AI type : temperature
+AI_TYPE = {'きっちり': 0.0, 'まぁまぁ': 0.5, 'クリエイティブ': 0.9}
+
+# SVG TEMPLATE
 SVG_HEADER = '''<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 720 720">
 <style>
     div { display: table; font-size: 16px; color: black; width: 70px; height: 80px; }
     p   { display: table-cell; text-align: center; vertical-align: middle;}
 </style>
 '''
-
-SVG_HEADER_OUTPUT = '''<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="720" height="720">
-<style>
-    foreignObject {font: 16px sans-serif; x: 2px; y: -8px; width: 72px; height: 80px; text-align: left; word-wrap: break-word;}
-    div { display: table; font-size: 16px; color: black; width: 70px; height: 80px;}
-    p   { display: table-cell; text-align: left; vertical-align: middle;}
-</style>
-'''
-
 SVG_ITEM = string.Template('''<g transform="translate($x,$y)">
     <rect x="0" y="0" width="80" height="80" fill="$color" stroke="gray"/>
     <foreignObject x="5" y="0" width="70" height="80">
@@ -54,12 +48,12 @@ SVG_ITEM = string.Template('''<g transform="translate($x,$y)">
     </foreignObject>
 </g>
 ''')
-
 SVG_FRAME = string.Template('''<g transform="translate($x,$y)">
     <rect x="0" y="0" width="$unit3" height="$unit3" fill="white" fill-opacity="0.0" stroke="black"/>
 </g>
 ''')
 
+# API PROMPT
 PROMPT = string.Template('''Answer 10 japanese keywords without NG words that you associate with this word. Answer should be Japanese:
 
 # word: $WORD
@@ -90,12 +84,6 @@ def association_words(word, temp, NG_words=[""]):
     result = res.replace("’", "'").replace("‘", "'").replace(";", "")
     return eval(result)
 
-def get_class_name(num):
-    result = ""
-    result += "inner" if num < 9 else "outer"
-    result += " center"  if num in range(4, 81, 9) else ""
-    return result
-
 def create_mandalachart(title, type_AI):
     temp = AI_TYPE[type_AI]
 # AI association word
@@ -114,17 +102,12 @@ def create_mandalachart(title, type_AI):
         words.insert(4, key)
         blocks.append(words)
 # create SVG
-    svg, svg_out = SVG_HEADER, SVG_HEADER_OUTPUT
+    svg = SVG_HEADER
     for y, row in enumerate(MANDAL_LIST):
         for x, num in enumerate(row):
             word = blocks[num // COL][num % COL]
             color = COLOR.get((x, y), C0)
             svg += SVG_ITEM.safe_substitute({
-                'x': x * UNIT, 'y': y * UNIT,
-                'word': word,
-                'color': color,
-            })
-            svg_out += SVG_ITEM.safe_substitute({
                 'x': x * UNIT, 'y': y * UNIT,
                 'word': word,
                 'color': color,
@@ -135,12 +118,8 @@ def create_mandalachart(title, type_AI):
         svg += SVG_FRAME.safe_substitute({
             'x': x * unit3, 'y': y * unit3, 'unit3': unit3
         })
-        svg_out += SVG_FRAME.safe_substitute({
-            'x': x * unit3, 'y': y * unit3, 'unit3': unit3
-        })
     svg += '</svg>'
-    svg_out += '</svg>'
-    return svg, svg_out
+    return svg
 
 # layout
 st.header("ＡＩが創るマンダラート")
@@ -153,16 +132,8 @@ mandala_svg, mandala_svg_output = "", ""
 if st.button('**マンダラート創造**') and title:
     try:
         with st.spinner("マンダラート創造中・・・30秒～数分程度お待ちください。"):
-            mandala_svg, mandala_svg_output = create_mandalachart(title, type_AI)
+            mandala_svg = create_mandalachart(title, type_AI)
             components.html(mandala_svg, height=720)
     except Exception as err:
         st.error(f'エラーが発生しました。　再度お試し下さい。')
         st.error(f'{err=}, {type(err)=}')
-
-# if mandala_svg_output:
-#     st.download_button(
-#         label="svgダウンロード【ダウンロードするとマンダラートは消えます】",
-#         data=mandala_svg_output,
-#         file_name='mandalachart.svg',
-#         mime='text/csv',
-#     )
